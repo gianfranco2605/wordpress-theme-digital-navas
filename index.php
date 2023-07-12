bd213b1e4800fa17ab2c96ed52a3fa9519e5c572
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-07-12 12:59:49 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Main Template File
+ * 
+ * @package DigitalNavas
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WordPress Site</title>
+</head>
+<body>
+    
+</body>
+</html>
