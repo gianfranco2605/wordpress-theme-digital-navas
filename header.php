b78985cc976b1df1f9cdf9abd1226d30c0430563
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-07-14 01:28:07 +0200

    adding some add_theme_support()

--- header.php
+++ header.php
@@ -11,8 +11,7 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php wp_head() ?><!-- scripts, styles -->
-        
+        <?php wp_head() ?><!-- scripts, styles -->     
     </head>
     <body <?php body_class(''); ?>> <!--function to include classes -->
 
--- inc/classes/class-digitalnavas-theme.php
+++ inc/classes/class-digitalnavas-theme.php
@@ -24,6 +24,57 @@ class DIGITALNAVAS_THEME {
         /**
          * Actions
         */
+        add_action('after_setup_theme', [ $this, 'setup_theme']);
+    }
+
+    public function setup_theme() {
+        //tittle function
+        add_theme_support( 'title-tag' );
+        //logo function
+        add_theme_support( 'custom-logo', [
+            'header-text' => [ 'site-title', 'site-description' ], 
+            'height' => 100,
+            'width' => 400,
+            'flex-height' => true,
+            'flex-width' => true,
+        ]);
+
+        // backgrounds
+        add_theme_support( 'custom-background', [
+            'default-color' => '#fff',
+            'default-image' => '',
+        ] );
+
+        // feature image
+        add_theme_support( 'post-thumbnails' );
+
+        // allows not to refresh the page on changes
+        add_theme_support( 'automatic-feed-links' );
+        
+        // allows wordpress to support this 
+        add_theme_support( 'html5', [
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+            'script',
+            'style',
+        ]);
+
+        add_editor_style();
+
+        add_theme_support( 'wp-block-styles' );
+
+        // options full width wide in guttenberg images 
+        add_theme_support( 'align-wide' );
+
+        global $content_width;
+        if( ! isset( $content_width ) ) {
+            $content_width = 1240;
+        }
+
+        
     }
 
 }
